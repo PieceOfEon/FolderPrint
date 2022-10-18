2291df22979bee000c0d4956bffcb9c6d33f9b74
PieceOfEon <danilskripka1996@gmail.com>
2022-10-18 14:59:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*irectoryInfo[] files=*/
+using System.IO;
+Console.WriteLine(@"D:\");
+ShowAllFoldersUnder(@"D:\Lineage2\", 0);
+static void ShowAllFoldersUnder(string path, int indent)
+{
+    try
+    {
+        foreach (string folder in Directory.GetDirectories(path))
+        {
+            Console.WriteLine("{0}{1}", new string(' ', indent), Path.GetFileName(folder));
+            string[] allfiles = Directory.GetFiles(folder);
+            foreach (string filename in allfiles)
+            {
+                Console.WriteLine("{0}{1}", new string(' ', indent), filename);
+            }
+            ShowAllFoldersUnder(folder, indent + 2);
+        }
+    }
+    catch (UnauthorizedAccessException) { }
+}
